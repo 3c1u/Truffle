67105a1b14f453b98c22da82752622adc03eb55d
shikugawa <shikugawa@gmail.com>
2021-01-04 10:52:12 +0900

    object

---
+++ object/button.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file      button.cpp
+ * @author    Rei Shimizu (shikugawa) <shikugawa@gmail.com>
+ * @brief     
+ *
+ * @copyright Copyright 2021 Rei Shimizu. All rights reserved.
+ */
+
+#include "button.h"
---
+++ object/button.h
@@ -0,0 +1,20 @@
+/**
+ * @file      button.h
+ * @author    Rei Shimizu (shikugawa) <shikugawa@gmail.com>
+ * @brief     
+ *
+ * @copyright Copyright 2021 Rei Shimizu. All rights reserved.
+ */
+
+#ifndef TRUFFLE_BUTTON_H
+#define TRUFFLE_BUTTON_H
+
+
+
+class button {
+
+};
+
+
+
+#endif //TRUFFLE_BUTTON_H
